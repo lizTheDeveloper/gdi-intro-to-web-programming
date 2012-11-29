7b8f43eb33ff5710effae809a1f8b1d80c460099
Liz Howard <ann.howard@me.com>
2012-11-29 01:33:38 -0500

    some code for processing forms, and a .gitignore file! yay!

--- examples/form_handler.js
+++ examples/form_handler.js
@@ -1,3 +1,4 @@
+
 //These are node modules. They allow you to use node.js's APIs
 var http = require('http'),
     url = require('url');
@@ -5,14 +6,20 @@ var http = require('http'),
 // http://nodejs.org/api/url.html - We'll be parsing the querystring with this.
 
 console.log('script running');
+var header ='<!doctype html><html><head><title>My Preprocessed App</title></head><body><div class="header"><h1>My Preprocessed Header</h1>';
+var aForm = '<form method="POST" action="/form">First Name:<input name="first_name"><br>Last Name:<input name="last_name"><button type="submit"></form>';
+var goBack ='<a href="/">Go Back Home</a>';
+var footer ='</body>';
 
-
-http.createServer(function (request,responder) {
+http.createServer(function (request,response) {
     console.log('server running');
-    //Recieved request start - begin listening for more data.
+    var path = url.parse(request.url).pathname;
+    var querystring = url.parse(request.url, true).query;  //return an object from a querystring with raw name-value pairs.
+
     request.addListener('data', function(chunk) {
        //handle a chunk of data, passed in as a buffer.
         request.formString += chunk.toString();
+        console.log(request.formString);
     });
     request.addListener('end', function() {
         //we're done, now parse the form.
@@ -23,36 +30,23 @@ http.createServer(function (request,responder) {
         console.log(request);
     });
 
-    var response = getResponse(request);
-
-    responder.writehead(response.code, response.header);
-    responder.write()
-
+    var body = makeBody(path, querystring, request);
+    response.writeHead(200, {
+    'Content-Length': body.length,
+    'Content-Type': 'text/html' });
+    response.end(body);
 }).listen(process.env.PORT);
 
 
-function getResponse(request) {
-    var path = url.parse(request.url).pathname;
-    var method = request.method;
-    var querystring = url.parse(request.url, true).query;
-
-    if (request.method == 'POST') {
-        request.
-    } else {
-        request.form == null;
-    }
-
-}
 
 
-
-var header ='<!doctype html><html><head><title>My Preprocessed App</title></head><body><div class="header"><h1>My Preprocessed Header</h1>';
-var goBack ='<a href="/">Go Back Home</a>';
-var footer ='</body>';
-
-function makeBody(path, querystring) {
+function makeBody(path, querystring, request) {
     var body = header;
     var name = querystring.name || 'lady';
+
+    if (request.method == "POST") {
+        console.log('Its post.');
+    }
 
     switch (path) {
         case '/greeting':
@@ -61,6 +55,10 @@ function makeBody(path, querystring) {
             break;
         case '/farewell':
             body += "Goodbye, " + name + ". :(";
+            body += goBack;
+            break;
+        case '/form':
+            body += aForm;
             body += goBack;
             break;
         default:
